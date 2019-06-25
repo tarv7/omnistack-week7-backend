5e4fe01595fdbdb76ace9cf904523665d10247e8
Thales Augusto <thallles.ramos@gmail.com>
2019-06-25 00:24:21 -0300

    Consertando problemas com socket.io

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const cors = require('cors');
 
 const app = express();
 
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const server = app.listen(3333);
+const io = require('socket.io').listen(server);
 
 mongoose.connect('mongodb+srv://semana:semana@cluster0-vbpha.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -23,5 +23,3 @@ app.use(cors());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
-
-app.listen(3333);
